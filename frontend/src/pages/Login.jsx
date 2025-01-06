54468bb4decc17422b8d715503c5c5a7274b57cd
Vijay Patidar <119438495+Vijaypatidar2003@users.noreply.github.com>
2025-01-06 16:06:33 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,6 +13,7 @@ const Login = () => {
 
   const submitHandler = (e) => {
     e.preventDefault();
+    localStorage.clear();
     axios
       .post("/users/login", { email: formData.email, password: formData.password })
       .then((res) => {
