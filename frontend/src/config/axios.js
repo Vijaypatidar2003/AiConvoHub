53cf116f9aef5d41a3a50752e27dacd6471501ee
Vijay Patidar <119438495+Vijaypatidar2003@users.noreply.github.com>
2025-01-07 12:51:10 +0530

    Update axios.js

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL:"https://aiconvohub-backend.onrender.com",
+    // baseURL:"https://aiconvohub-backend.onrender.com",
+    baseURL:"https://aiconvohub-backend.vercel.app/",
     headers:{
         "Authorization":`Bearer ${localStorage.getItem('token')}`
     }
