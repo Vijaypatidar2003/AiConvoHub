0f7b5ef3aebf1c2c5f25ab1344de35d31919c798
Vijay Patidar <119438495+Vijaypatidar2003@users.noreply.github.com>
2025-01-09 15:50:33 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser())
 // app.use(cors());
 app.use(cors({
-     origin: 'https://aiconvohub-frontend.onrender.com',
+     origin: 'http://localhost:300',
    
     credentials: true,
 }));
